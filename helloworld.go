22d47c4db0af0d15bf94950af1066b90f3ab6d1d
Karmugilan <sughasini.karmugilan@kpn.com>
2023-03-06 17:13:23 +0100

    adding test kubeconfig

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,24 @@
-package main  
- 
-import "fmt" 
- 
-func main() {  
-   fmt.Println("Hello World")  
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := filepath.Join(os.Getenv("HOMEPATH"), ".kube", "config")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Error creating client: %s", err.Error())
+	}
+	fmt.Println(clientset)
 }
